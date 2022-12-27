208d8f117008274a64b9adff36f4c220aafa2117
naleeyyy <kahmeti123@gmail.com>
2022-12-27 11:04:20 +0100

    line and draw triangle finished

--- laps.c/laps.c
+++ laps.c/laps.c
@@ -11,10 +11,11 @@ int main(int argc, char* argv[]) {
     uint32_t pixels[WIDTH*HEIGHT];
     
     laps_fill(pixels, WIDTH, HEIGHT, BACKGROUND_COLOR);
-    // laps_draw_rectange(pixels, WIDTH/2, HEIGHT/2, 100, 100, WIDTH, HEIGHT, 0xFFFF00FF);
+    laps_fill_rectange(pixels, WIDTH, HEIGHT, WIDTH/2, HEIGHT/2, 100, 100, 0xFFFF00FF);
     // laps_draw_circle(pixels, WIDTH/2, HEIGHT/2, 25, WIDTH, HEIGHT, 0xFF00FFFF);
     // laps_draw_line(pixels, 0, 0, HEIGHT, WIDTH, WIDTH, HEIGHT, 0xFF00FFFF);
-    laps_draw_line(pixels, 0, 0, WIDTH, HEIGHT, WIDTH, HEIGHT, FOREGROUND_COLOR);
+    // laps_draw_line(pixels, WIDTH, HEIGHT, 0, 0, WIDTH, HEIGHT, 0xFFFF00FF);
+    laps_draw_triangle(pixels, WIDTH, HEIGHT, 0, 0, 20, 100, 200, 200, 0xFFFF00FF);
     laps_save_to_ppm(pixels, WIDTH, HEIGHT, "test.ppm");
 
     return 0;
